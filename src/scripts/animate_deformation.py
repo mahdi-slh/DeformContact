0532fb10042341f5ab0e750b41a589a9eab26623
Mahdi Saleh <m.saleh@tum.de>
2023-09-29 21:30:42 +0200

    added data animator

---
+++ src/scripts/animate_deformation.py
@@ -0,0 +1,60 @@
+import open3d as o3d
+import numpy as np
+import os
+import shutil
+import time
+import pathlib
+
+def animate_meshes(initial_mesh, def_mesh, num_frames, extrapolation_frames):
+    assert initial_mesh.has_vertices() and def_mesh.has_vertices(), "Meshes must have vertices"
+    assert len(initial_mesh.vertices) == len(def_mesh.vertices), "Meshes must have the same number of vertices"
+    
+    initial_vertices = np.asarray(initial_mesh.vertices)
+    def_vertices = np.asarray(def_mesh.vertices)
+    
+    interpolated_meshes = []
+    
+    total_frames = num_frames + extrapolation_frames
+    
+    for i in range(total_frames):
+        if i < num_frames:
+            alpha = i / (num_frames - 1)  # Linear interpolation parameter between initial and def meshes.
+        else:
+            alpha = 1 + (i - num_frames + 1) / extrapolation_frames  # Extrapolation parameter beyond def mesh.
+        
+        interpolated_vertices = (1 - alpha) * initial_vertices + alpha * def_vertices
+        interpolated_mesh = o3d.geometry.TriangleMesh()
+        interpolated_mesh.vertices = o3d.utility.Vector3dVector(interpolated_vertices)
+        interpolated_mesh.triangles = initial_mesh.triangles
+        interpolated_meshes.append(interpolated_mesh)
+        
+    return interpolated_meshes
+
+
+        
+if __name__ == "__main__":
+    directory_path = "../datasets/everyday_deform/deformations/Cat/"
+    file_name_resting = "InitialMesh.ply"
+    file_name_deformed = "2023_09_14__11_02_10"
+    
+    # Create a new directory to store the animated meshes
+    parent_directory_path = pathlib.Path(directory_path).parent
+    output_dir = os.path.join(parent_directory_path, "Cat_animate")
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Copy the JSON file to the new directory
+    json_file_name = file_name_deformed + ".json"
+    shutil.copy(os.path.join(directory_path, json_file_name), os.path.join(output_dir, json_file_name))
+    
+    def_mesh = o3d.io.read_triangle_mesh(os.path.join(directory_path, file_name_deformed+'.ply'))
+    initial_mesh = o3d.io.read_triangle_mesh(os.path.join(directory_path, file_name_resting))
+    
+    num_interpolation_frames = 10
+    num_extrapolation_frames = 5
+    
+    mesh_list = animate_meshes(initial_mesh, def_mesh, num_interpolation_frames, num_extrapolation_frames)
+    
+    # Save all the resulting meshes to the new directory
+    for idx, mesh in enumerate(mesh_list): # You can use this as a unique identifier for each file, representing the time of interpolation/extrapolation
+        file_name = f"{file_name_deformed}_{idx}.ply"
+        o3d.io.write_triangle_mesh(os.path.join(output_dir, file_name), mesh)
